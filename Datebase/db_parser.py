8f0d6fc3646c9dbda7d0b81624bb3bc64ff397e9
SiniyRG <github_kari@mail.ru>
2022-09-01 11:01:41 +0300

    Create_tables

--- Datebase/db_parser.py
+++ Datebase/db_parser.py
@@ -25,54 +25,7 @@ try:
                 f'''TRUNCATE TABLE {table} RESTART IDENTITY CASCADE'''
             )
     # Таблица сериалов
-    # with connection.cursor() as cursor:
-    #     cursor.execute(
-    #         '''CREATE TABLE serials
-    #         (id_serial serial PRIMARY KEY,
-    #         name_ru varchar(75) NOT NULL,
-    #         name_en varchar(75) UNIQUE,
-    #         description text NOT NULL);'''
-    #     )
-    #     print('[Инфо]Создана таблица "serials"')
-    #
-    # with connection.cursor() as cursor:
-    #     cursor.execute(
-    #         '''CREATE TABLE countries
-    #         (id_country serial PRIMARY KEY,
-    #         name_country varchar(75) UNIQUE);'''
-    #     )
-    #     print('[Инфо]Создана таблица "countries"')
-    #
-    # with connection.cursor() as cursor:
-    #     cursor.execute(
-    #         '''CREATE TABLE genres
-    #         (id_genre serial PRIMARY KEY,
-    #         name_genre varchar(30) UNIQUE);'''
-    #     )
-    #     print('[Инфо]Создана таблица "genres"')
-    # with connection.cursor() as cursor:
-    #     cursor.execute(
-    #         '''CREATE TABLE genre_series
-    #         (id_serial integer REFERENCES serials (id_serial) ON DELETE CASCADE,
-    #         id_genre integer REFERENCES genres (id_genre) ON DELETE CASCADE);'''
-    #     )
-    #     print('[Инфо]Создана таблица "genre_series"')
-    # with connection.cursor() as cursor:
-    #     cursor.execute(
-    #         '''CREATE TABLE country_series
-    #         (id_serial integer REFERENCES serials (id_serial) ON DELETE CASCADE,
-    #         id_country integer REFERENCES countries (id_country) ON DELETE CASCADE); '''
-    #     )
-    #     print('[Инфо]Создана таблица "country_series"')
-    #
-    # with connection.cursor() as cursor:
-    #     cursor.execute(
-    #         '''CREATE TABLE output_series
-    #         (id_serial integer REFERENCES serials (id_serial) ON DELETE CASCADE,
-    #          number_serial varchar(10) NOT NULL,
-    #          release text NOT NULL);'''
-    #     )
-    #     print('[Инфо]Создана таблица "output_series"')
+
 
     for parsing_value in parsing_values:
         # Добавление данных в таблицу сериалов
